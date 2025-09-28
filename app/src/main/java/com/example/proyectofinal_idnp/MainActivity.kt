c63cb83727691c9dbf013a5ad70f4cdd0ff57cce
Gabriela <gmartellv@unsa.edu.pe>
2025-09-27 21:21:02 -0500

    commit 1 - pantalla con botón

---
+++ app/src/main/java/com/example/proyectofinal_idnp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.proyectofinalidnp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val boton = findViewById<Button>(R.id.botonEnviar)
+        boton.setOnClickListener {
+
+        }
+    }
+}
